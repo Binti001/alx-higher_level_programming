af0883b64c673c50259730896a83a134a1281994
Binti001 <sharonwafula3@gmail.com>
2022-12-10 12:21:56 -0800

    add 2 tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    if len(tuple_a) == 1 and len(tuple_b) >= 2:
+        arg1 = tuple_a[0] + tuple_b[0]
+        arg2 = 0 + tuple_b[1]
+    if len(tuple_b) == 1 and len(tuple_a) >= 2:
+        arg1 = tuple_a[0] + tuple_b[0]
+        arg2 = tuple_a[1] + 0
+    if len(tuple_a) >= 2 and len(tuple_b) >= 2:
+        arg1 = tuple_a[0] + tuple_b[0]
+        arg2 = tuple_a[1] + tuple_b[1]
+    if len(tuple_a) < 1 and len(tuple_b) >= 2:
+        arg1 = 0 + tuple_b[0]
+        arg2 = 0 + tuple_b[1]
+    if len(tuple_b) < 1 and len(tuple_a) >= 2:
+        arg1 = tuple_a[0] + 0
+        arg2 = tuple_a[1] + 0
+    if len(tuple_a) < 1 and len(tuple_b) < 1:
+        arg1 = 0 + 0
+        arg2 = 0 + 0
+    if len(tuple_a) == 1 and len(tuple_b) == 1:
+        arg1 = tuple_a[0] + tuple_b[0]
+        arg2 = 0 + 0
+
+    return arg1, arg2
