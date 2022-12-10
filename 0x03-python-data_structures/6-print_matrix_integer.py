881521a9377a5f584c20283f7cddb4a38d56e40d
Binti001 <sharonwafula3@gmail.com>
2022-12-10 11:03:05 -0800

    print a matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,6 @@
 
 def print_matrix_integer(matrix=[[]]):
 
-  for row in matrix:
+    for row in matrix:
 
-    print(" ".join("{:d}".format(num) for num in row))
+        print(" ".join("{:d}".format(num) for num in row))
