c2599e302434c66b77d91860a095e2e521559577
Binti001 <sharonwafula3@gmail.com>
2023-01-02 23:38:42 -0800

    dividewq 2 integers and prints the result

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@
 def safe_print_division(a, b):
     try:
         x = a / b
-    except ZeroDivisionError:
+    except (TypeError, ZeroDivisionError):
         x = None
     finally:
         print("Inside result:{}".format(x))
