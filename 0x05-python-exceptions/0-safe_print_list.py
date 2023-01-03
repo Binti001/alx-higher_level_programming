742383da9e27b25430dd01c7ae7a10373162f7e9
Binti001 <sharonwafula3@gmail.com>
2023-01-02 18:29:02 -0800

    print x elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for i in my_list[:x]:
+            print("{}".format(i), end="")
+            count += 1
+    except IndexError:
+        for i in my_list:
+            print("{}".format(i), end="")
+            count += 1
+    print("\n")
+    return count
---
+++ 0x05-python-exceptions/header.h
@@ -0,0 +1,12 @@
+#ifndef _HEADER_H_
+#define _HEADER_H_
+
+def safe_print_list(my_list=[], x=0):
+def safe_print_integer(value):
+def safe_print_list_integers(my_list=[], x=0):
+def safe_print_division(a, b):
+def list_division(my_list_1, my_list_2, list_length):
+def raise_exception():
+def raise_exception_msg(message=""):
+
+#endif
