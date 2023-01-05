0be470d2de9ff6f13a4ff30a96adf8128cb9644e
Binti001 <sharonwafula3@gmail.com>
2023-01-04 19:03:28 -0800

    print the class Square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -16,7 +16,6 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-
         if type(position) is not tuple or len(position) != 2 or type(position[0]) is not int or type(position[1]) is not int or position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
