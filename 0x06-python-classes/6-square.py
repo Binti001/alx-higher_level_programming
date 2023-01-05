5225ebecfea300766868336101677418bac05a13
Binti001 <sharonwafula3@gmail.com>
2023-01-04 19:01:05 -0800

    print the class Square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -24,10 +24,12 @@ class Square:
 
     @property
     def size(self):
+        """getter of size"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """setter of size"""
         if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -37,19 +39,23 @@ class Square:
 
     @property
     def position(self):
+        """getter of size"""
         return self.__positon
     
     @position.setter
     def position(self, value):
+        """setter of size"""
         if type(value) is not tuple or len(value) != 2 or type(value[0]) is not int or type(value[1]) is not int or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
 
     def area(self):
+        """returns the current square area"""
         return self.__size * self.__size
 
     def my_print(self):
+        """prints in stdout the square with the character #"""
         if self.__size == 0:
             print()
         else:
