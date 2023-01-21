a12052461c4cbee1fbcd4857ac0fec553bcde565
Binti001 <sharonwafula3@gmail.com>
2023-01-21 14:58:11 -0800

    print a list, but sorted (ascending sort)

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,13 +3,12 @@
 
 
 class MyList(list):
-    """class that inherits from the built-in list class"""
+    """public instance method that sorts the list and prints it"""
     def print_sorted(self):
-        """public instance method that sorts the list and prints it"""
-        a = self.copy()
         """Create a copy of the list so that the original is not modified"""
+        a = self.copy()
+        """Sort the list in ascending order"""
         for i in range(len(a) - 1):
-            """Sort the list in ascending order"""
             if a[i] > a[i+1]:
                 temp = a[i]
                 a[i] = a[i+1]
