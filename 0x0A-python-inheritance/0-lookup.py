cef2b5e684c6dd9a195018fc21f4044f82a877ff
Binti001 <sharonwafula3@gmail.com>
2023-01-21 13:38:12 -0800

    return the list of available attributes and methods of an object

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 
-
-
 def lookup(obj):
+    """Create an empty list"""
     attr_meth = []
+    """Loop through the object"""
     for i in dir(obj):
+        """Append the item to the list"""
         attr_meth.append(i)
+    """return the list"""
     return attr_meth
